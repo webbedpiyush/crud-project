fd2b091905ed9c375078fd710d063c7f52c13125
piyush <piyushtiwarindls220@gmail.com>
2023-12-31 17:33:46 +0530

    maded frontend

--- crudapp/src/screens/Home.js
+++ crudapp/src/screens/Home.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <div>
-      <div><Navbar/></div>
-      <div>Body</div>
-      <div><Footer/></div>
+      <div>
+        <Navbar />
+      </div>
+      <div><Carousel/></div>
+     <div className="m-3"><Card/></div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
