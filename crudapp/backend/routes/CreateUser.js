29937652dfbb390a06bbf2a1622f4de11879cafb
piyush <piyushtiwarindls220@gmail.com>
2024-01-03 20:30:49 +0530

    replaced zod in the place of express-validator

--- crudapp/backend/routes/CreateUser.js
+++ crudapp/backend/routes/CreateUser.js
@@ -2,17 +2,44 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require('express-validator');
+const zod = require('zod');
 
-router.post('/createuser' ,[
+const emailSchema = zod.string().email();
+const nameSchema = zod.string().min(5);
+const passwordSchema = zod.string().min(5);
+const locationSchema = zod.string();
+
+async function middlewareZod (req,res,next) {
+  const userEmail = req.body.email;
+  const userName = req.body.name;
+  const userpassword = req.body.password;
+  const userLocation = req.body.location;
+  const safeEmail = emailSchema.safeParse(userEmail);
+  const safeName = nameSchema.safeParse(userName);
+  const safepassword = passwordSchema.safeParse(userpassword);
+  const safeLocation = locationSchema.safeParse(userLocation);
+
+  if(!(safeEmail.success) || !(safeName.success) || !(safepassword.success) || !(safeLocation.success)) {
+    res.status(400).json({
+      message:"wrong inputs"
+    })
+    return;
+  }
+  next();
+} 
+
+router.post('/createuser' ,
+/*[
 body('email').isEmail(),
 body('name').isLength({min:5}),
-body('password','Incorrect Password').isLength({min:5}) ]
-,async function(req,res) {
+body('password','Incorrect Password').isLength({min:5}) ]*/
 
-  const err = validationResult(req);
+middlewareZod ,async function(req,res) {
+
+ /* const err = validationResult(req);
   if(!err.isEmpty()) {
     return res.status(400).json({ err: err.array() });
-  }
+  }*/
 
   try{
     await User.create({
